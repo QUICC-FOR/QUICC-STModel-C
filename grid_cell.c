f0897ac4da574a044c5d517e4e973ec37cc8987c
SteveViss <s.vissault@yahoo.fr>
2014-06-26 17:09:56 -0400

    Fix bug: all prob sum to 1

--- grid_cell.c
+++ grid_cell.c
@@ -24,7 +24,7 @@ StateData * transProb; // output
             case TRANSITIONAL:
             	// the line below is an error, and it is repeated for each type
             	// is it not possible that a state will stay the same from one time step to the next?
-                cell->transitionProbs[TRANSITIONAL] =0; // remember, these are functions which will depend on the climate of the cell in question
+                cell->transitionProbs[TRANSITIONAL] =1 - (phi_c(cell->climate)+phi_d(cell->climate)+phi_m(cell->climate)); // remember, these are functions which will depend on the climate of the cell in question
                 cell->transitionProbs[CONIFEROUS] = phi_c(cell->climate);
                 cell->transitionProbs[DECIDUOUS] = phi_d(cell->climate);
                 cell->transitionProbs[MIXED] = phi_m(cell->climate);
@@ -34,19 +34,19 @@ StateData * transProb; // output
                 cell->transitionProbs[TRANSITIONAL] = epsi(cell->climate);
                 cell->transitionProbs[CONIFEROUS] = theta_c(cell->climate);
                 cell->transitionProbs[DECIDUOUS] = theta_d(cell->climate);
-                cell->transitionProbs[MIXED] = 0;
+                cell->transitionProbs[MIXED] = 1-(epsi(cell->climate)+theta_c(cell->climate)+theta_d(cell->climate));
                 break;
 
             case DECIDUOUS:
                 cell->transitionProbs[TRANSITIONAL] = epsi(cell->climate);
                 cell->transitionProbs[CONIFEROUS] = 0;
-                cell->transitionProbs[DECIDUOUS] = 0;
+                cell->transitionProbs[DECIDUOUS] = 1 - (epsi(cell->climate)+beta_c(cell->climate)*(cell->prevalence[CONIFEROUS]+cell->prevalence[MIXED]));
                 cell->transitionProbs[MIXED] = beta_c(cell->climate)*(cell->prevalence[CONIFEROUS]+cell->prevalence[MIXED]);
                 break;
 
             case CONIFEROUS:
                 cell->transitionProbs[TRANSITIONAL] = epsi(cell->climate);
-                cell->transitionProbs[CONIFEROUS] = 0;
+                cell->transitionProbs[CONIFEROUS] = 1 - (epsi(cell->climate)+beta_d(cell->climate)*(cell->prevalence[MIXED]+cell->prevalence[DECIDUOUS]));
                 cell->transitionProbs[DECIDUOUS] = 0;
                 cell->transitionProbs[MIXED] = beta_d(cell->climate)*(cell->prevalence[MIXED]+cell->prevalence[DECIDUOUS]);
                 break;
