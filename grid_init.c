ad0128a859b6798245c7e07cd7a82ecbbb07ce62
SteveViss <s.vissault@yahoo.fr>
2014-07-10 15:41:00 +0200

    finish set_disturb_grid and add if statement in set_random_grid

--- grid_init.c
+++ grid_init.c
@@ -4,45 +4,29 @@
 void set_random_grid(Grid* grid){
 
     // **DESCRIPTION**: Set full random grid of three states: CONIFEROUS,
-    // DECIDUOUS, MIXED. Remove state: TRANSITIONAL
+    // DECIDUOUS, MIXED.
 
     for (y, y < grid->yDim, y++) {
         for (x, x < grid->xDim, x++) {
 
             // Assign location cell value in currentCell
             GridCell * currentCell = grid_get_cell(x,y);
+            int rValue = gsl_rng_uniform_int(rng, unsigned long int 3);
 
             // Set state based on the random value
-
-            // RETRIEVE for IF
-            switch((int)(gsl_rng_set(rng, (int) * 10) )) // Number between 0 and 100
-                {
-                    case 25:        // this is 25 <= rng_value < 50
-                            currentCell->currentState = currentCell->stateHistory = CONIFEROUS;
-                        break;
-                    case 50:        // this is 50 <= rng_value < 75
-                            currentCell->currentState = currentCell->stateHistory = DECIDUOUS;
-                        break;
-                    case 75:        // this is 75 <= rng_value
-                            currentCell->currentState = currentCell->stateHistory = MIXED;
-                        break;
-                    default:
-                        abort();
-                        break;
-                }
+            if(rValue == 0){
+                        currentCell->currentState = currentCell->stateHistory = CONIFEROUS;
+            } else if (rValue == 1) {
+                        currentCell->currentState = currentCell->stateHistory = DECIDUOUS;
+            } else {
+                        currentCell->currentState = currentCell->stateHistory = MIXED;
+            }
 
             set_disturb_grid();
             gsl_rng_free(rng);
         }
     }
 }
-
-//void set_disturb_grid( Grid* grid){
-
-    // DESCRIPTION:  The idea behind this function aims to create a grid with
-    // randomly a proportion of patch disturbed in the landscape
-
-//}
 
 void set_uniform_grid(Grid* grid){
 
@@ -81,14 +65,20 @@ void set_disturb_grid( Grid* grid, double thresDist){
 
     double thresDist = 0.20; // Set
 
-    int totalCell = grid->yDim * grid->yDim; // Get total number of cells
-    int numDist = totalCell * thresDist; // Get number of cells disturbed based on threshold
+    int totalCells = grid->yDim * grid->yDim; // Get total number of cells
+    int ysize = grid->yDim
+    int xsize = grid->xDim
+    int numDist = totalCells * thresDist; // Get number of cells disturbed based on threshold
 
     for (i, i < numDist, i ++){
-        // get random x coord
-        // get random y coord
 
-        GridCell * currentCell = grid_get_cell(x,y);
+        int ryCoord = gsl_rng_uniform_int(rng, unsigned long int ysize);
+        int rxCoord = gsl_rng_uniform_int(rng, unsigned long int xsize);
+
+        GridCell * currentCell = grid_get_cell(rxCoord,ryCoord);
         currentCell->currentState = currentCell->stateHistory = TRANSITIONAL;
     }
+
+    gsl_rng_free(rng);
+
 }
