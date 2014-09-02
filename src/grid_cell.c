57df6ad5ac26fdb1c2d62380a2ba1b264441966b
Philippe Desjardins-Proulx <philippe.d.proulx@gmail.com>
2014-09-01 20:07:53 -0400

    format

--- src/grid_cell.c
+++ src/grid_cell.c
@@ -5,114 +5,132 @@
 #include "grid.h"
 
 // prototypes for functions defining model parameters
-static double beta_d (Climate climate);
-static double beta_c (Climate climate);
-static double theta_c (Climate climate);
-static double theta_d (Climate climate);
-static double phi_d (Climate climate);
-static double phi_c (Climate climate);
-static double phi_m (Climate climate);
-static double epsi  (Climate climate);
+static double beta_d(Climate climate);
+static double beta_c(Climate climate);
+static double theta_c(Climate climate);
+static double theta_d(Climate climate);
+static double phi_d(Climate climate);
+static double phi_c(Climate climate);
+static double phi_m(Climate climate);
+static double epsi(Climate climate);
 
+void gc_get_trans_prob(GridCell *cell) {
+  /*
+          calculate the transition probabilities given the current state
+          values are automatically stored in cell->transitionProbs
+  */
 
-void gc_get_trans_prob(GridCell* cell)
-{
-	/*
-		calculate the transition probabilities given the current state
-		values are automatically stored in cell->transitionProbs
-	*/
+  switch (*(cell->currentState)) {
 
-	switch ( *(cell->currentState) ) {
+  case TRANSITIONAL:
+    cell->transitionProbs[DECIDUOUS] = phi_d(cell->climate);
+    cell->transitionProbs[CONIFEROUS] = phi_c(cell->climate);
+    cell->transitionProbs[MIXED] = phi_m(cell->climate);
+    cell->transitionProbs[TRANSITIONAL] =
+        1 - (cell->transitionProbs[MIXED] + cell->transitionProbs[CONIFEROUS] +
+             cell->transitionProbs[DECIDUOUS]);
+    break;
 
-		case TRANSITIONAL:
-			cell->transitionProbs[DECIDUOUS] = phi_d(cell->climate);
-			cell->transitionProbs[CONIFEROUS] = phi_c(cell->climate);
-			cell->transitionProbs[MIXED] = phi_m(cell->climate);
-			cell->transitionProbs[TRANSITIONAL] = 1 -(cell->transitionProbs[MIXED] + cell->transitionProbs[CONIFEROUS] + cell->transitionProbs[DECIDUOUS]);
-			break;
+  case MIXED:
+    cell->transitionProbs[DECIDUOUS] = theta_d(cell->climate);
+    cell->transitionProbs[CONIFEROUS] = theta_c(cell->climate);
+    cell->transitionProbs[TRANSITIONAL] = epsi(cell->climate);
+    cell->transitionProbs[MIXED] = 1 - (cell->transitionProbs[TRANSITIONAL] +
+                                        cell->transitionProbs[CONIFEROUS] +
+                                        cell->transitionProbs[DECIDUOUS]);
+    break;
 
-		case MIXED:
-			cell->transitionProbs[DECIDUOUS] = theta_d(cell->climate);
-			cell->transitionProbs[CONIFEROUS] = theta_c(cell->climate);
-			cell->transitionProbs[TRANSITIONAL] = epsi(cell->climate);
-			cell->transitionProbs[MIXED] = 1 - (cell->transitionProbs[TRANSITIONAL] + cell->transitionProbs[CONIFEROUS] + cell->transitionProbs[DECIDUOUS]);
-			break;
+  case DECIDUOUS:
+    cell->transitionProbs[CONIFEROUS] = 0;
+    cell->transitionProbs[TRANSITIONAL] = epsi(cell->climate);
+    cell->transitionProbs[MIXED] =
+        beta_c(cell->climate) *
+        (cell->prevalence[CONIFEROUS] + cell->prevalence[MIXED]);
+    cell->transitionProbs[DECIDUOUS] =
+        1 -
+        (cell->transitionProbs[TRANSITIONAL] + cell->transitionProbs[MIXED]);
+    break;
 
-		case DECIDUOUS:
-			cell->transitionProbs[CONIFEROUS] = 0;
-			cell->transitionProbs[TRANSITIONAL] = epsi(cell->climate);
-			cell->transitionProbs[MIXED] = beta_c(cell->climate)*(cell->prevalence[CONIFEROUS]+cell->prevalence[MIXED]);
-			cell->transitionProbs[DECIDUOUS] = 1 - (cell->transitionProbs[TRANSITIONAL] + cell->transitionProbs[MIXED]);
-			break;
+  case CONIFEROUS:
+    cell->transitionProbs[DECIDUOUS] = 0;
+    cell->transitionProbs[TRANSITIONAL] = epsi(cell->climate);
+    cell->transitionProbs[MIXED] =
+        beta_d(cell->climate) *
+        (cell->prevalence[MIXED] + cell->prevalence[DECIDUOUS]);
+    cell->transitionProbs[CONIFEROUS] =
+        1 -
+        (cell->transitionProbs[TRANSITIONAL] + cell->transitionProbs[MIXED]);
+    break;
 
-		case CONIFEROUS:
-			cell->transitionProbs[DECIDUOUS] = 0;
-			cell->transitionProbs[TRANSITIONAL] = epsi(cell->climate);
-			cell->transitionProbs[MIXED] = beta_d(cell->climate)*(cell->prevalence[MIXED]+cell->prevalence[DECIDUOUS]);
-			cell->transitionProbs[CONIFEROUS] = 1 - (cell->transitionProbs[TRANSITIONAL] + cell->transitionProbs[MIXED]);
-			break;
-
-			default:
-				break;
-	}
+  default:
+    break;
+  }
 }
 
-
-GridCell *gc_make_cell (size_t numTimeSteps) {
-	// allocates memory for the state history, initializes other values to 0
-	// initializes the current state to the first record in stateHistory
-	GridCell *cell = (GridCell *) malloc(sizeof (GridCell));
-	cell->stateHistory = (State *) malloc(numTimeSteps * sizeof (State));
-	assert(cell->stateHistory);
-	cell->currentState = cell->stateHistory;
-	cell->historySize = numTimeSteps;
-	return cell;
+GridCell *gc_make_cell(size_t numTimeSteps) {
+  // allocates memory for the state history, initializes other values to 0
+  // initializes the current state to the first record in stateHistory
+  GridCell *cell = (GridCell *)malloc(sizeof(GridCell));
+  cell->stateHistory = (State *)malloc(numTimeSteps * sizeof(State));
+  assert(cell->stateHistory);
+  cell->currentState = cell->stateHistory;
+  cell->historySize = numTimeSteps;
+  return cell;
 }
 
-
-void gc_destroy_cell (GridCell* cell) {
-	cell->currentState = NULL;
-	free(cell->stateHistory);
-	free(cell);
+void gc_destroy_cell(GridCell *cell) {
+  cell->currentState = NULL;
+  free(cell->stateHistory);
+  free(cell);
 }
 
+void gc_select_new_state(GridCell *cell, gsl_rng *rng) {
+  double rValue = gsl_rng_uniform(rng);
+  double testVal = 0;
+  State newState = 0;
+  State GC_POSSIBLE_STATES[GC_NUM_STATES];
 
+  for (int i = 0; i < GC_NUM_STATES; i++) {
+    State curState = GC_POSSIBLE_STATES[i];
+    testVal += cell->transitionProbs[curState];
+    if (rValue < testVal) {
+      newState = curState;
+      break;
+    }
+  }
 
-void gc_select_new_state(GridCell* cell, gsl_rng* rng)
-{
-	double rValue = gsl_rng_uniform(rng);
-	double testVal = 0;
-	State newState = 0;
-	State GC_POSSIBLE_STATES [GC_NUM_STATES];
+  cell->currentState++;
 
-	for(int i = 0; i < GC_NUM_STATES; i++) {
-		State curState = GC_POSSIBLE_STATES[i];
-		testVal += cell->transitionProbs[curState];
-		if(rValue < testVal) {
-			newState = curState;
-			break;
-		}
-	}
+  // check that we haven't wandered into invalid memory
+  assert(cell->currentState < (cell->stateHistory + (cell->historySize - 1)));
 
-	cell->currentState++;
-
-	// check that we haven't wandered into invalid memory
-	assert(cell->currentState < (cell->stateHistory + (cell->historySize - 1)));
-
-	*(cell->currentState) = newState;
+  *(cell->currentState) = newState;
 }
-
-
 
 // TO IMPLEMENT
 // FOR NOW, THESE RETURN A DUMMY VALUE; EVENTUALLY WILL RETURN THE RESULTS OF
 // STATISTICAL MODELS
-static double beta_d (Climate climate) { return 0.5; }
-static double beta_c (Climate climate) { return 0.5; }
-static double theta_c (Climate climate) { return 0.25; }
-static double theta_d (Climate climate) { return 0.25; }
-static double phi_d (Climate climate) { return 0.2; }
-static double phi_c (Climate climate) { return 0.2; }
-static double phi_m (Climate climate) { return 0.2; }
-static double epsi  (Climate climate) { return 0.1; }
-
+static double beta_d(Climate climate) {
+  return 0.5;
+}
+static double beta_c(Climate climate) {
+  return 0.5;
+}
+static double theta_c(Climate climate) {
+  return 0.25;
+}
+static double theta_d(Climate climate) {
+  return 0.25;
+}
+static double phi_d(Climate climate) {
+  return 0.2;
+}
+static double phi_c(Climate climate) {
+  return 0.2;
+}
+static double phi_m(Climate climate) {
+  return 0.2;
+}
+static double epsi(Climate climate) {
+  return 0.1;
+}
--- src/main.c
+++ src/main.c
@@ -14,29 +14,27 @@
 #define MAX_TIME 100;
 #define OUTPUT_FILE "test_output.csv"
 
+int main(int argc, char **argv) {
 
-int main(int argc, char ** argv) {
+  // set up RNG
+  gsl_rng *rng = gsl_rng_alloc(gsl_rng_mt19937);
+  assert(rng);
+  gsl_rng_set(rng, (int)time(NULL));
 
-    // set up RNG
-    gsl_rng* rng = gsl_rng_alloc(gsl_rng_mt19937);
-    assert(rng);
-    gsl_rng_set(rng, (int) time(NULL));
+  // set up the grid
+  Grid *grid = gr_make_grid(10, 10, 100, UNIFORM, rng);
+  gr_view_grid(grid);
 
-    // set up the grid
-    Grid * grid = gr_make_grid(10, 10, 100, UNIFORM, rng);
-    gr_view_grid(grid);
+  // main loop in time
+  for (int year = 0; year < 100; year++) {
+    gr_update_grid(grid);
+  }
 
-    // main loop in time
-    for(int year = 0; year < 100; year++) {
-         gr_update_grid(grid);
-    }
+  // io_write_output(grid, OUTPUT_FILE);
 
-    //io_write_output(grid, OUTPUT_FILE);
+  // cleanup
+  // gr_destroy_grid(grid);
+  // gsl_rng_free(rng);
 
-    // cleanup
-    //gr_destroy_grid(grid);
-    //gsl_rng_free(rng);
-
-    return 0;
+  return 0;
 }
-
