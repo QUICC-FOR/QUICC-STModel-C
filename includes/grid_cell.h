afd6a6dcac92c604ff97892e3c95f845dd479bc5
SteveViss <s.vissault@yahoo.fr>
2014-09-04 16:20:06 -0400

    Delete unused extern variable declared

--- includes/grid_cell.h
+++ includes/grid_cell.h
@@ -10,10 +10,8 @@
 #define GC_NUM_STATES 4
 typedef enum { DECIDUOUS,CONIFEROUS,TRANSITIONAL,MIXED} State;
 typedef double StateData [GC_NUM_STATES];
-
 typedef enum { MOORE, VONNE} NeighType;
 
-extern State GC_POSSIBLE_STATES [GC_NUM_STATES];  // this is an array giving all possible states
 
 typedef struct {
     double meanTemp;
