ef6deb9fa37101911897142686b19b41b8ea38cc
SteveViss <s.vissault@yahoo.fr>
2014-09-07 16:15:40 -0400

    Finalize main loop

--- src/main.c
+++ src/main.c
@@ -11,30 +11,51 @@
 
 // length of time to run the model
 
-#define  MAX_TIME   100
+#define  MAX_TIME   4
+#define  GR_SIZE    10
+#define NEIGHTYPE MOORE
 #define OUTPUT_FILE "test_output.csv"
+
 
 int main(int argc, char **argv) {
 
-  // set up RNG
+// set up RNG
   gsl_rng *rng = gsl_rng_alloc(gsl_rng_mt19937);
   assert(rng);
   gsl_rng_set(rng, (int)time(NULL));
 
-  // set up the grid
-  Grid *grid = gr_make_grid(20, 20, 100, UNIFORM, rng);
-  gr_view_grid(grid);
+// set up the grid
+  Grid *grid = gr_make_grid(GR_SIZE, GR_SIZE, MAX_TIME, UNIFORM, rng);
 
-  // main loop in time
+// main loop in time
   for (int year = 0; year < MAX_TIME; year++) {
-    gr_update_grid(grid);
+    
+    // View grid
+    gr_view_grid(grid);
+    printf(" T ------------------------------------------------ \n");
+    for(int x =0; x < GR_SIZE; x++){
+        for(int y =0; y < GR_SIZE; y++){
+
+            //Compute prevalence
+            gr_compute_prevalence(grid, x, y,  NEIGHTYPE);
+
+            //Compute trans probabilities
+            gc_get_trans_prob (gr_get_cell(grid,x,y));
+            gc_select_new_state (gr_get_cell(grid,x,y), rng);
+        }
+    }
+
+    // View grid
+    printf(" T+1 ------------------------------------------------ \n");
+    gr_view_grid(grid);
+
   }
 
   // io_write_output(grid, OUTPUT_FILE);
 
   // cleanup
-  // gr_destroy_grid(grid);
-  // gsl_rng_free(rng);
+ gr_destroy_grid(grid);
+ gsl_rng_free(rng);
 
   return 0;
 }
