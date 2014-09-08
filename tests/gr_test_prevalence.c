766d85982a6fca7cb8dc24fb6d018f51103c9ae9
SteveViss <s.vissault@yahoo.fr>
2014-09-08 15:14:43 -0400

    Add new test: gr_test_prevalence; Switch name of some tests gc_ to gr_

---
+++ tests/gr_test_prevalence.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+#include <time.h>
+#include <gsl/gsl_rng.h>
+
+#include "../src/grid_cell.c"
+#include "../src/grid.c"
+
+
+int main() {
+
+	/* DESC:  Tests on prevalence function */
+
+	
+
+	
+}
