1f32407e93399fa6c7e94823a2763600d8cc9d6f
SteveViss <s.vissault@yahoo.fr>
2014-09-12 17:49:06 -0400

    Last work on gc_test_alloc.c

--- tests/gc_test_alloc.c
+++ tests/gc_test_alloc.c
@@ -6,32 +6,32 @@
 
 int main() {
 
-	/* DESC: creates a cell, assigns values to it, and tries to get them back out */ 
-	
-	// Init cell
-	size_t histSize = 10;
-	GridCell * cell = gc_make_cell(histSize);
-	
-	// Define first state
-	*(cell->currentState) = DECIDUOUS;
+    /* DESC: creates a cell, assigns values to it, and tries to get them back out */ 
+    
+    // Init cell
+    size_t histSize = 10;
+    GridCell * cell = gc_make_cell(histSize);
+    
+    // Define first state
+    *(cell->currentState) = DECIDUOUS;
 
-	// Assertion of the state and the cell
-	assert(cell->stateHistory[0] == DECIDUOUS);
-	assert(cell->historySize == histSize);
+    // Assertion of the state and the cell
+    assert(cell->stateHistory[0] == DECIDUOUS);
+    assert(cell->historySize == histSize);
 
-	// Fill state history
-	for(int i = 1; i < histSize; i++) {
-		cell->currentState++;
-		*(cell->currentState) = TRANSITIONAL;
-	}
+    // Fill state history
+    for(int i = 1; i < histSize; i++) {
+        cell->currentState++;
+        *(cell->currentState) = TRANSITIONAL;
+    }
 
-	// Assert value of the last time step
-	assert(cell->stateHistory[histSize-1] == TRANSITIONAL);
-	
-	// Basic writing test in cell struct
-	cell->climate.meanTemp = 0;
-	cell->prevalence[0] = cell->transitionProbs[0] = 0.5;
-	
-	// Free memory
-	gc_destroy_cell(cell);
+    // Assert value of the last time step
+    assert(cell->stateHistory[histSize-1] == TRANSITIONAL);
+    
+    // Basic writing test in cell struct
+    cell->climate.meanTemp = 0;
+    cell->prevalence[0] = cell->transitionProbs[0] = 0.5;
+    
+    // Free memory
+    gc_destroy_cell(cell);
 }
