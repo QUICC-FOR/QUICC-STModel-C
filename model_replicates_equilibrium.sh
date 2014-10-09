bfaf0a75119ab65a66d0707e905fcb51bfca06bc
SteveViss <s.vissault@yahoo.fr>
2014-10-09 14:12:09 -0400

    Upgrade bash file with variables

--- model_replicates_equilibrium.sh
+++ model_replicates_equilibrium.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-mkdir -p outDir
+# Simulation parameters
+WritingStep=10
+nSimu=100
+Timesteps=1000
 
-for y in {1..100}
+outDir=Times$Timesteps-Rep$nSimu
+
+mkdir -p $outDir
+
+intWritingStep=${Timesteps}/${WritingStep}
+
+for (( n=1; n<=${nSimu}; n++ ))
 do
-	mkdir -p outDir/simu_$y
-	printf "Running simulation $y of 100
+	mkdir -p $outDir/simu_$n
+	printf "Running simulation $n of 100
 	......................................\n"
 
-	for t in {1..100}
+	for (( t=1; t <=${intWritingStep}; t++ ))
 	do
-		present_outfile=outDir/simu_$y/out$t.csv
+		present_outfile=$outDir/simu_$n/$(( $t * $WritingStep ))_step_out.csv
 		if [ $t -eq 1 ]
 		then
-			printf "Running timestep 	$(( $t*10 )) of 1000\n"
-			./build/stmodel -x 600 -y 1500 -a 60 -b 150 -c ./PastClimate70-00.csv -s -p ./dompars.txt -t 10 -d 0.2 >$present_outfile 2>/dev/null
+			printf "Running timestep 	$(( $t * $WritingStep )) of $Timesteps \n"
+			./build/stmodel -x 600 -y 1500 -a 60 -b 150 -c ./PastClimate70-00.csv -s -p ./dompars.txt -t $WritingStep -d 0.2 >$present_outfile 2>/dev/null
 		else
-			printf "                       $(( $t*10 )) of 1000\n"
-			previous_outfile=outDir/simu_$y/out$(( $t - 1 )).csv
-			./build/stmodel -x 600 -y 1500 -a 60 -b 150 -c ./PastClimate70-00.csv -s -p ./dompars.txt -t 10 -d 0.0 -g $previous_outfile >$present_outfile 2>/dev/null
+			printf "                        $(( $t * $WritingStep )) of $Timesteps \n"
+			previous_outfile=$outDir/simu_$n/$(( $t * $WritingStep - $WritingStep ))_step_out.csv
+			./build/stmodel -x 600 -y 1500 -a 60 -b 150 -c ./PastClimate70-00.csv -s -p ./dompars.txt -t $WritingStep -d 0.0 -g $previous_outfile >$present_outfile 2>/dev/null
 		fi
 	done
 done
