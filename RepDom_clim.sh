9c830bbb35bbc32d1e887bbb11358053fd336dc3
SteveViss <s.vissault@yahoo.fr>
2014-10-18 11:48:37 -0400

    Clean repo CANUSA

--- RepDom_clim.sh
+++ RepDom_clim.sh
@@ -3,9 +3,9 @@
 # Simulation parameters
 WritingStep=10
 nSimu=1
-Timesteps=1000
+Timesteps=100
 
-outDir=Times$Timesteps-Rep$nSimu-RealParam-CorrecLines-CANUSA
+outDir=Times$Timesteps-Rep$nSimu-DomPars-ClimRepDom
 
 mkdir -p $outDir
 
@@ -23,11 +23,11 @@ do
 		if [ $t -eq 1 ]
 		then
 			printf "Running timestep 	$(( $t * $WritingStep )) of $Timesteps \n"
-			./build/stmodel -x 600 -y 840 -a 60 -b 84 -c ./PastClimate70-00.csv -s -p ./dompars2.txt -t $WritingStep -d 0.1 >$present_outfile 2>/dev/null
+			./build/stmodel -x 600 -y 840 -a 60 -b 84 -c ./futureClimate_4.csv -s -p ./dompars.txt -t $WritingStep -d 0.1 >$present_outfile 2>/dev/null
 		else
 			printf "                        $(( $t * $WritingStep )) of $Timesteps \n"
 			previous_outfile=$outDir/simu_$n/$(( $t * $WritingStep - $WritingStep ))_step_out.csv
-			./build/stmodel -x 600 -y 840 -a 60 -b 84 -c ./PastClimate70-00.csv -s -p ./dompars2.txt -t $WritingStep -d 0.0 -g $previous_outfile >$present_outfile 2>/dev/null
+			./build/stmodel -x 600 -y 840 -a 60 -b 84 -c ./futureClimate_4.csv -s -p ./dompars.txt -t $WritingStep -d 0.0 -g $previous_outfile >$present_outfile 2>/dev/null
 		fi
 	done
 done
