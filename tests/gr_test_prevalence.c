7b0e3d595e446d9f8bec19fa0d4ec4a2aa1a736e
SteveViss <s.vissault@yahoo.fr>
2015-02-03 17:41:45 -0500

    Fix issue with gr_test_prevalence

--- tests/gr_test_prevalence.c
+++ tests/gr_test_prevalence.c
@@ -13,6 +13,7 @@ int main() {
 	*/
 
 	int testResult = 0;
+	short globalPrevalence =0;
 
 	// verify that st_state_to_index fails properly
 	testResult += (st_state_to_index('D') != -1);
@@ -20,14 +21,14 @@ int main() {
 	// test both neighborhood types
 	Grid * testGr = gr_make_grid(3, 3, MOORE, UNIFORM, 0, NULL);
 	StateData prevalence;
-	gr_compute_local_prevalence(testGr, 1, 1, prevalence,0);
+	gr_compute_local_prevalence(testGr, 1, 1, prevalence);
 	testResult += prevalence[st_state_to_index('T')] != 3.0/8.0;
 	testResult += prevalence[st_state_to_index('B')] != 3.0/8.0;
 	testResult += prevalence[st_state_to_index('M')] != 2.0/8.0;
 	testResult += prevalence[st_state_to_index('R')] != 0;
 
 	testGr = gr_make_grid(3, 3, VONNE, UNIFORM, 0, NULL);
-	gr_compute_local_prevalence(testGr, 1, 1, prevalence,0);
+	gr_compute_local_prevalence(testGr, 1, 1, prevalence);
 	testResult += prevalence[st_state_to_index('T')] != 1.0/4.0;
 	testResult += prevalence[st_state_to_index('B')] != 1.0/4.0;
 	testResult += prevalence[st_state_to_index('M')] != 2.0/4.0;
@@ -37,13 +38,13 @@ int main() {
 	testGr = gr_make_grid(3, 3, MOORE, UNIFORM, 0, NULL);
 	testGr->stateCurrent[1][2] = 'R';
 	double epsilon = 0.0000001;
-	gr_compute_local_prevalence(testGr, 2, 2, prevalence,0);
+	gr_compute_local_prevalence(testGr, 2, 2, prevalence);
 	testResult += prevalence[st_state_to_index('T')] != 0;
 	testResult += prevalence[st_state_to_index('B')] != 0;
 	testResult += fabs(prevalence[st_state_to_index('M')] - 2.0/3.0) > epsilon;
 	testResult += fabs(prevalence[st_state_to_index('R')] - 1.0/3.0) > epsilon;
 
-	gr_compute_local_prevalence(testGr, 0, 1, prevalence,0);
+	gr_compute_local_prevalence(testGr, 0, 1, prevalence);
 	testResult += fabs(prevalence[st_state_to_index('T')] - 2.0/5.0) > epsilon;
 	testResult += fabs(prevalence[st_state_to_index('B')] - 1.0/5.0) > epsilon;
 	testResult += fabs(prevalence[st_state_to_index('M')] - 1.0/5.0) > epsilon;
